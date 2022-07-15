d5ff564ac165dfb753e231f15ce8e4b4bfc21a2e
agustin-sobrero <agussobrero@hotmail.com>
2022-07-14 21:10:50 -0300

    Clase 6 || Servidores Web

---
+++ clase6/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+const PORT= process.env.PORT || 3000
+
+const Container = require("./container")
+const container = new Container ("products.json")
+
+const server = app.listen(PORT, (req, res) => {
+    console.log(`Servidor esuchando el puerto ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1>Funciona?</h1>")
+})
+
+app.get("/productos", (req, res) =>{
+    container.getAll().then(
+        products => res.send(products)
+    )
+})
+
+app.get("/productoRandom", (req,res) =>{
+    container.getRandom().then(
+        product=> res.send(product)
+    )
+})
