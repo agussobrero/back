d125d0e3ca4f7de3d9b1f7e31f555ea71c4d0875
agustin-sobrero <agussobrero@hotmail.com>
2022-08-03 22:15:33 -0300

    Clase 10 | Motores de Plantillas hbs/prob

--- clase10/handlebars/server.js
+++ clase10/handlebars/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
+const Container = require("./utils/container")
 const app = express()
 const productos = []
+
+const container = new Container ("./utils/products.json")
 
 const hbs = handlebars.create({
     extname: ".hbs",
@@ -21,17 +24,69 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.post("/productos", (req, res) => {
-    productos.push(req.body)
-    console.log(req.body)
-    res.redirect("/")
+app.get("/productos", async(req, res) => {
+    let productos 
+    try {
+        productos = await container.getAll()
+    } catch (err) {
+        console.log(err)
+    }
+    res.render("main", {productos})
 })
 
-app.get("/productos", (req, res) => {
-    res.render("tabla", {productos})
+app.post("/productos", async (req, res) => {
+    console.log(req.body)
+    const producto = req.body
+    console.log(req.body)
+    console.log(producto)
+    let prodNuevo
+    try{
+        prodNuevo = await container.save(producto)
+    } catch (err) {
+        console.log(err)
+    }
+    res.redirect("/productos")
 })
+
+/* app.get("/productos", (req, res) => {
+    res.render("tabla", {productos})
+}) */
 
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Conencted to PORT: ${PORT}`)
 })
+
+
+/* socket.on('productosActualizado', (dato) => {
+    fetch('http://localhost:8080/table.handlebars')
+        .then(data => data.text())
+        .then(response => {
+            let nuevoRender = Handlebars.compile(response);
+            let html = nuevoRender({ listaDeProductos: dato });
+            document.getElementById('destino').innerHTML = html;
+        })
+
+        ...
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new socket...
+...
+
+app.use('/api/productos', rutas);
+app.get('/', (req, res) => {
+    res.render('./table', {lista...});
+})
+
+
+io.on('connection', (socket) => {
+
+    cargarProductos(); //envia productos cuando alguien se conecta
+    
+    . ...
+    socket.on('nuevoMensaje', (dato) => {
+        ...
+
+    })
+})
+}) */
