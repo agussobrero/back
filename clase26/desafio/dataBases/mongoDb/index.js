ca32312d9de95f8d635347a9eb082c475e2d339d
agustin-sobrero <agussobrero@hotmail.com>
2022-10-20 16:30:52 -0300

    Clase26 | Autenticacion

---
+++ clase26/desafio/dataBases/mongoDb/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const DATABASE_CLUSTER= process.env.DATABASE_CLUSTER
+const DATABASE_USER = process.env.DATABASE_USER
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
+const DATABASE_NAME = process.env.DATABASE_NAME
+const DATABASE_CLI = process.env.DATABASE_CLI
+
+connection = async () => {
+    const URIString = "mongodb+srv://agussobrero:xpZFIMrxekWrDNMA@cluster0.7znzdl8.mongodb.net/authies?retryWrites=true&w=majority"
+    try{
+        await mongoose.connect(URIString)
+        console.log("Conectado a MongoDb")
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+connection() 
+
+module.exports = connection
